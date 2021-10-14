51ce3b7d9a02ef1e58207358da15ccfbb622a8c1
Quan Ngo <quanngo@acadiau.ca>
2021-10-13 22:24:19 -0300

    blog works locally

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
-    // eleventyConfig.addPassthroughCopy("./src/css/");
-    // eleventyConfig.addWatchTarget("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/assets/");
+    // eleventyConfig.addWatchTarget("./src");
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_MED)
+});
     return {
         dir: {
             input: "src",
